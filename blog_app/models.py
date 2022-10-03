ce791f7cda224b5b960179e0fc21006996b53669
musawakiliML <musawakiliml@hotmail.com>
2022-10-03 08:20:58 +0100

    fixed an error in Post model

--- blog_app/models.py
+++ blog_app/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_add_now=True)
+    created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     
