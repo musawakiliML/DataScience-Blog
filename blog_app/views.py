326595bc97232e44d919b593df76b7ebb8e6ae73
Musa Adamu Wakili <musawakiliml@hotmail.com>
2023-10-09 16:01:24 +0100

    Update

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home_page_view(request):
+    return HttpResponse("Welcome to Data Science Blog")
