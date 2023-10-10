4500efbcdfef116ad0d9416a64c4b7ef080a45b2
Musa Adamu Wakili <musawakiliml@hotmail.com>
2023-10-10 11:12:52 +0100

    Update: Added templates

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import home_page_view
+from .views import home_page_view, HomePageView
 
 urlpatterns = [
-    path("", home_page_view, name="home page view"),
+    # path("", home_page_view, name="home_page_view"),
+    path("", HomePageView.as_view(), name="home")
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 
 def home_page_view(request):
     return HttpResponse("Welcome to Data Science Blog")
+
+class HomePageView(TemplateView):
+    template_name = 'index.html'
